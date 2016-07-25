b6aef824ed481b282f4cd2224fc1d0f39bfe01e9
Alex Lytvak <Litvak82@bigmir.net>
2016-07-25 21:32:31 +0300

    add lab

---
+++ LabSerealizator/LabSerealizator/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+
+namespace LabSerealizator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Vasya", "a1234", "OOP", 23, 4.5);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("student.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, student1);
+
+                Console.WriteLine("Объект сериализован Binary");
+            }
+
+            using (FileStream fs = new FileStream("student.dat", FileMode.OpenOrCreate))
+            {
+                Student newPerson = (Student)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован ");
+                Console.WriteLine("Name: {0} --- ID: {1} --TEAM :{2}---CODE :{3}--MARK :{4}",student1.name,student1.id,student1.team,student1.code,student1.mark);
+            }
+
+            SoapFormatter formatter2 = new SoapFormatter();
+            
+            using (FileStream fs = new FileStream("student.soap", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, student1);
+
+                Console.WriteLine("Объект сериализован SOAP");
+            }
+
+            XmlSerializer formattexml = new XmlSerializer(typeof(Student));
+
+           
+            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, student1);
+
+                Console.WriteLine("Объект сериализован xml");
+            }
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LabSerealizator/LabSerealizator/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace LabSerealizator
+{
+     [Serializable]
+   public  class Student
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string team { get; set; }
+        public int code { get; set; }
+        public double mark { get; set; }
+
+        public Student(string name, string id, string team, int code, double mark)
+        {
+            this.name = name;
+            this.id = id;
+            this.team = team;
+            this.code = code;
+            this.mark = mark;
+        }
+        public Student()
+        {
+            name = "none";
+            id = "none";
+            team = "none";
+            code = 0;
+            mark = 0;
+        }
+    }
+}
